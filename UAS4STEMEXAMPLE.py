b29bfff592440442be621bfc0e3df19780a18d0d
BrotherBei <132795672+BrotherBei@users.noreply.github.com>
2023-08-19 13:28:02 -0500

    FlightControl.py

--- UAS4STEMEXAMPLE.py
+++ UAS4STEMEXAMPLE.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-
 from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative, Command
 from pymavlink import mavutil # Needed for command message definitions
 import time
